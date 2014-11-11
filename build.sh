7d062fd2ef13577103da724b5fe3131957ec7a76
Christo DeLange <cdelange@amplify.com>
2014-11-11 13:23:32 -0800

    Rename zip file

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -pv build
-zip -r build/ruby-example.zip * -x "build/" "build.sh" "common.sh" "deploy.sh" "post-deploy.py" "int-tests.sh"
+zip -r build/v4mars.zip * -x "build/" "build.sh" "common.sh" "deploy.sh" "post-deploy.py" "int-tests.sh"
