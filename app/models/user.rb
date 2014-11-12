4c342643def68d76f0c8bf2d8f02b3dd0736c292
A.J. Supinski <aj@Admins-MacBook-Air.local>
2014-11-11 18:57:50 -0800

    get soe more logging

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      puts "trying to insert the user #{auth}"
       #twitter doesn’t provide the email so just skip
       #user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
