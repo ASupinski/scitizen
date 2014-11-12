e2cdea04e7c7873409652b10de53aa34a5250693
Christo DeLange <cdelange@amplify.com>
2014-11-11 17:29:43 -0800

    Show seed progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,15 +40,17 @@ def get_images_for_sol(sol_url)
   return image_urls
 end
 
+counter = 0
 ret = RestClient.get main_url
 json_data = JSON.parse(ret)
-json_data.each do |_,data|
+json_data.each do |mission,data|
   mission_ret = RestClient.get data['image_manifest']
   mission_data = JSON.parse(mission_ret)
   sols_data = mission_data['sols']
 
   images = []
   sols_data.each do |sol_data|
+    puts "#{counter += 1}: Mission: #{mission}, Sol: #{sol_data['url']}"
     sol_image_urls = get_images_for_sol sol_data['url']
     images.concat(sol_image_urls)
   end
