74486a80357a56fbeda6aad8b2ca436e2a4d210b
Rossserino <Serino.ross@gmail.com>
2014-11-11 23:05:43 -0500

    Updated image tags query

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -34,11 +34,12 @@ class ExploreController < ApplicationController
   end
   
   def gallery_by_tag
-    @images = Image.joins(:image_tags).where(:tag => params['tag']).paginate(page: params[:page], per_page: 15).order('created_at DESC')
+    @images = Image.joins(:image_tags).where({image_tags.tag => params['tag']}).paginate(page: params[:page], per_page: 15).order('created_at DESC')
     respond_to do |format|
       format.html
       format.js
     end
+	render "gallery"
   end
 
   def list_anno
